6d93314eed22bcee97debbf6863fe0566599958f
Pawlo2020 <45106252+Pawlo2020@users.noreply.github.com>
2018-11-28 13:48:16 +0100

    28.11.2018

--- Basu/Basu/ViewModel/DataLoader.cs
+++ Basu/Basu/ViewModel/DataLoader.cs
@@ -6,18 +6,24 @@ using System.Threading.Tasks;
 using System.Windows.Controls;
 namespace Basu.ViewModel
 {
-    class Arrive
-    {
-        int ID;
-        int hour;
-        int minute;
-    }
+    
 
     class DataLoader
     {
+        private Dictionary<string, string> Dict;
         private Model.Line _line;
         private Model.BusStop _busStop;
-        private Model.Timetable _timetable;
+        //private Model.Timetable _timetable;
+
+        public void getData()
+        {
+            //Get data from XML
+            //parse hours and minutes to Dictionary
+
+            Dict = new Dictionary<string, string>();
+
+
+        }
 
         public void setTimeTable(ListView lista)
         {
--- Basu/Basu/ViewModel/DataSetter.cs
+++ Basu/Basu/ViewModel/DataSetter.cs
@@ -8,5 +8,17 @@ namespace Basu.ViewModel
 {
     class DataSetter
     {
+        class Item
+        {
+            int ID;
+            int hour;
+            int minute;
+        }
+
+
+
+
+
+
     }
 }
